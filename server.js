104d7e54a7f74ba154ba799b56832080410db959
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-09-11 00:13:55 +0500

    css aplication assets file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser")
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +11,16 @@ const PORT = process.env.PORT||8080
 
 //log requests
 app.use(morgan("tiny"));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+//set view engine
+app.set("view engine", "ejs");
+// app.set("views", path.resolve(__dirname, "views/ejs"))
+
+//load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")))
 
 app.get("/", (req,res) => {
     res.send("Crud Application");
