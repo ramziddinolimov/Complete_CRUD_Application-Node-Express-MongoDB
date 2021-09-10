ca658d97208941736c3d11c51a1d93c69f079516
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-09-11 00:33:35 +0500

    index ejs render server

--- server.js
+++ server.js
@@ -20,10 +20,12 @@ app.set("view engine", "ejs");
 // app.set("views", path.resolve(__dirname, "views/ejs"))
 
 //load assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")))
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.get("/", (req,res) => {
-    res.send("Crud Application");
+    res.render("index")
 })
 
 app.listen(PORT, () =>{console.log(`Server is running on http://localhost:${PORT}`)});
