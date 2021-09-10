ed4f5666da0276d67e848ec5940480c459825146
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-09-10 23:59:54 +0500

    morgan

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 const app = express();
+
+dotenv.config({path:"config.env"})
+const PORT = process.env.PORT||8080
+
+//log requests
+app.use(morgan("tiny"));
 
 app.get("/", (req,res) => {
     res.send("Crud Application");
 })
 
-app.listen(3000, () =>{console.log(`Server is running on http://localhost:${3000}`)});
+app.listen(PORT, () =>{console.log(`Server is running on http://localhost:${PORT}`)});
