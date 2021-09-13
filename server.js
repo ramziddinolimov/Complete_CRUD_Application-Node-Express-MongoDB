ecd40f307cf50d8a8861113f55e169b26552c781
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-09-14 03:09:11 +0500

    routes continued

--- server.js
+++ server.js
@@ -24,19 +24,7 @@ app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
-app.get("/", (req,res) => {
-    res.render("index")
-})
-
-
-app.get("/add-user", (req,res) => {
-    res.render("add_user")
-})
-
-
-app.get("/update-user", (req,res) => {
-    res.render("update_user")
-})
+app.use('/', require('./server/routes/router'))
 
 
 app.listen(PORT, () =>{console.log(`Server is running on http://localhost:${PORT}`)});
---
+++ server/routes/router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const route = express.Router()
+
+const services = require('../services/render');
+
+
+/**
+ * @description
+ * @method GET
+ */
+
+route.get('/', services.homeRoutes);
+
+/**
+ * @description
+ * @method GET
+ */
+
+route.get('add_user', services.add_user);
+
+/**
+ * @description
+ * @method GET
+ */
+
+route.get('update_user', services.update_user);
+
+
+
+module.exports = route
