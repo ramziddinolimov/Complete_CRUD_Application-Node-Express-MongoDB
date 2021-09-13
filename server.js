e4e047d92bf79c773d516c9b3fca0692e125e0b7
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-09-14 02:33:04 +0500

    ejs finished

--- server.js
+++ server.js
@@ -28,4 +28,15 @@ app.get("/", (req,res) => {
     res.render("index")
 })
 
+
+app.get("/add-user", (req,res) => {
+    res.render("add_user")
+})
+
+
+app.get("/update-user", (req,res) => {
+    res.render("update_user")
+})
+
+
 app.listen(PORT, () =>{console.log(`Server is running on http://localhost:${PORT}`)});
