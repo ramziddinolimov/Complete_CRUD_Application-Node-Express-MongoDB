b8dc7467b0030e244484300ca7e1efa144e47c25
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-18 23:13:21 +0500

    model js started

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const bodyparser = require("body-parser")
 const path = require("path");
 
+const connectDB = require('./server/database/connection');
+
 const app = express();
 
 dotenv.config({path:"config.env"})
@@ -11,6 +13,9 @@ const PORT = process.env.PORT||8080
 
 //log requests
 app.use(morgan("tiny"));
+
+//mongodb connection
+connectDB();
 
 //parse request to body-parser
 app.use(bodyparser.urlencoded({extended:true}));
