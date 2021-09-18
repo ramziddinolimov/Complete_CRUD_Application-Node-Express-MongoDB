a807c25740b489ab50837da787d764dec5e8b9eb
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-19 01:21:16 +0500

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -41,7 +41,24 @@ exports.find = (req,res) => {
 
 //Update a new idetified user by user id
 exports.update = (req,res) => {
+    if(!req.body){
+        return res 
+        .status(400)
+        .send({message: "Data to update can not be empty"});
+    }
 
+    const id = req.params.id;
+    Userdb.findByIdAndUpdate(id, req.body, {useFindAndModify:false})
+    .then(data => {
+        if(!data){
+            res.status(404).send({message: `Cannot Update user with ${id}.Maybe user not found`})
+        } else {
+            res.send(data)
+        }
+    })
+    .catch(err=>{
+        res.status(500).send({message: "Error Update user information"})
+    })
 }
 
 //Delete a user with specified user id in the requeste
