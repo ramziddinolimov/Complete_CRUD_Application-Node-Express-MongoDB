6335b7527293381dbd662c3b66f4a40c387b9072
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-18 23:58:21 +0500

    model js continued

--- server/routes/router.js
+++ server/routes/router.js
@@ -28,6 +28,9 @@ route.get('/update_user', services.update_user);
 
 //API
 route.post('/api/users', controller.create);
+route.get('/api/users', controller.find);
+route.put('/api/users/:id', controller.update);
+route.delete('/api/users/:id', controller.delete);
 
 
 
