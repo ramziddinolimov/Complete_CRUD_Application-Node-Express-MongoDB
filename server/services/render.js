9affcd06c2c8d33eb66dfe5845fd1064dd0a0d6f
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-19 16:08:14 +0500

    show and index ejs upload

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,14 @@
+const axios = require('axios');
+
 exports.homeRoutes = (req,res) => {
-    res.render('index', {users: "New Data"});
+    axios.get('http://localhost:3000/api/users')
+    .then(function(response) {
+        res.render('index',{users: response.data});
+    })
+    .catch(err=>{
+        res.send(err);
+    })
+
 }
 
 
