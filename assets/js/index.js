e9b832a301877c4f087986e02cf5587a2fc5b5a7
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-09-19 17:23:40 +0500

    index js update_users continuedd

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,10 @@
 $("#add_user").submit(function (event) {
     alert("Data Inserted Successfully!");
 })
+
+$("#update_user").submit(function (event) {
+    event.preventDefault();
+
+    var unindexed_array = $(this).serializeArray();
+    console.log(unindexed_array)
+})
